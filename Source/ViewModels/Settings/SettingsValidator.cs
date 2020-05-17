189d98255398bff205030f529d41f07eed5032e5
Peter <petey@poops.me>
2020-05-17 16:46:14 +0200

    Reword the settings validation messages

--- Source/ViewModels/Settings/SettingsValidator.cs
+++ Source/ViewModels/Settings/SettingsValidator.cs
@@ -11,7 +11,7 @@ namespace FastBuild.Dashboard.ViewModels.Settings
 		{
 			if (!Directory.Exists(brokeragePath))
 			{
-				return new ValidationResult("brokerage path not existed", new[] { nameof(SettingsViewModel.BrokeragePath) });
+				return new ValidationResult("Brokerage path doesn't exist", new[] { nameof(SettingsViewModel.BrokeragePath) });
 			}
 
 			return ValidationResult.Success;
@@ -23,7 +23,7 @@ namespace FastBuild.Dashboard.ViewModels.Settings
 			{
 				if (!File.Exists(editorPath))
 				{
-					return new ValidationResult("specified editor path not existed",
+					return new ValidationResult("Specified editor path does not exist",
 						new[] {nameof(SettingsViewModel.ExternalSourceEditorPath)});
 				}
 			}
@@ -31,7 +31,7 @@ namespace FastBuild.Dashboard.ViewModels.Settings
 			{
 				if (!IoC.Get<IExternalSourceEditorService>().IsSelectedEditorAvailable)
 				{
-					return new ValidationResult("the editor cannot be found at your Program Files, please locate it here",
+					return new ValidationResult("The editor cannot be found in Program Files, please locate it here",
 						new[] { nameof(SettingsViewModel.ExternalSourceEditorPath) });
 				}
 			}
